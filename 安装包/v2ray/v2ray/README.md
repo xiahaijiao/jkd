636a0cb50e3cf6f0cbd93900691b80db72ac2fbd
xiahaijiao <haijiao.xia@h-shgroup.com>
2024-12-10 14:03:27 +0800

    aa

---
+++ 安装包/v2ray/v2ray/README.md
@@ -0,0 +1,14 @@
+
+https://github.com/v2fly/fhs-install-v2ray/tree/develop
+https://github.com/xiahaijiao/fhs-install-v2ray
+
+# docker
+```bash
+安装
+https://github.com/v2fly/fhs-install-v2ray
+docker
+https://github.com/v2fly/docker
+源码
+https://github.com/v2fly/v2ray-core
+
+```
---
+++ 安装包/v2ray/v2ray/run.sh
@@ -0,0 +1,9 @@
+bash -x install-release-my.sh
+\cp -f server.json  /usr/local/etc/v2ray/config.json
+
+#wget https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+#download_v2ray
+
+#v2ray-linux.zip  -- 
+# v2ray-linux-64.zip  ,amd64
+# v2ray-linux-arm64-v8a.zip ,arm64
